9572533d9a22be0fd0821a271f677b290fc2f4d2
henripalin <henri.j.palin@gmail.com>
2021-01-21 12:09:46 +0200

    add names.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import data from './names.json'
 
 function App() {
+  const names = data.names
+  for (var i = 0; i < names.length; i++) {
+    console.log(names[i].name)
+  }
   return (
     <div>
       <p>Hello World!</p>
