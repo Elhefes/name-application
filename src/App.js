61b0e4c6099b75cfd0beefb851dd9f0f677ea17a
henripalin <henri.j.palin@gmail.com>
2021-01-21 15:41:42 +0200

    add sort buttons

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import data from './names.json'
 function App() {
   const [names, setNames] = useState(data.names)
   const [filter, setFilter] = useState('')
-  const mostPopularFirst = (a, b) => b.amount - a.amount
-  const alphabetically = (a, b) => a.name.localeCompare(b.name)
+  const [sortBy, setSortBy] = useState('popularity')
+
+  const byPopularity = (a, b) => b.amount - a.amount
+  const byName = (a, b) => a.name.localeCompare(b.name)
 
   useEffect(() => {
     setNames(data.names.filter(item => item.name.toLowerCase().includes(filter)))
@@ -18,28 +20,30 @@ function App() {
   return (
     <div>
       <Header />
-      <p>These are the top-10 male and female names in Solita.</p>
+      <p>These are the top 10 male and female names in Solita.</p>
       <Filter setFilter={setFilter} />
-      <Total names={names} />
-
-      <table>
-        <tbody>
-          <tr>
-            <th>
-              <button>Name⤊</button>
-            </th>
-            <th>
-            <button>Amount⤊</button>
-            </th>
-          </tr>
-          {names.sort(mostPopularFirst).map((item, index) =>
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.amount}</td>
+      <div style={{ marginTop: 20 }}>
+        <table>
+          <tbody>
+            <tr>
+              <th>
+                <button onClick={() => setSortBy('name')}>Name</button>
+              </th>
+              <th>
+                <button onClick={() => setSortBy('popularity')}>Amount</button>
+              </th>
             </tr>
-          )}
-        </tbody>
-      </table>
+            {names.sort(sortBy === 'popularity' ?
+              byPopularity : byName).map((item, index) =>
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.amount}</td>
+                </tr>
+              )}
+          </tbody>
+        </table>
+      </div>
+      <Total names={names} />
     </div>
   )
 }
